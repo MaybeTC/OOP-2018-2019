86d5e13013ed25d96a98d165b8b5de7b6cb67cff
MaybeTC <d16122910@mydit.ie>
2019-03-21 03:31:14 +0800

    bla‘

--- java/src/ie/dit/Product.java
+++ java/src/ie/dit/Product.java
@@ -7,9 +7,15 @@ public class Product
     private String name;
     private float price;
 
-    public Product(String name, float f)
+    public Product(String name, float price)
     {
+        this.name = name;
+        this.price = price;
+    }
 
+    public String toString()
+    {
+        return name + "\t" + price;
     }
 
     public Product(TableRow tr);
